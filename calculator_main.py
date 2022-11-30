2b311e3ae886378b5d117be39a3ab556f8304249
Chisu-Park <117416299+Chisu-Park@users.noreply.github.com>
2022-12-01 02:20:15 +0900

    Update calculator_main.py

--- calculator_main.py
+++ calculator_main.py
@@ -11,19 +11,14 @@ class Main(QDialog):
 
         ### 각 위젯을 배치할 레이아웃을 미리 만들어 둠
         layout_operation = QGridLayout()
-        layout_clear_equal = QGridLayout()
         layout_number = QGridLayout()
         layout_equation_solution = QFormLayout()
 
         ### 수식 입력과 답 출력을 위한 LineEdit 위젯 생성
-        label_equation = QLabel("Equation: ")
-        label_solution = QLabel("Number: ")
         self.equation = QLineEdit("")
-        self.solution = QLineEdit("")
 
         ### layout_equation_solution 레이아웃에 수식, 답 위젯을 추가
-        layout_equation_solution.addRow(label_equation, self.equation)
-        layout_equation_solution.addRow(label_solution, self.solution)
+        layout_equation_solution.addRow(self.equation)
 
         ### 사칙연상 버튼 생성
         button_plus = QPushButton("+")
@@ -51,9 +46,9 @@ class Main(QDialog):
 
         ### 추가연산 버튼을 클릭했을 때, 각 사칙연산 부호가 수식창에 추가될 수 있도록 시그널 설정
         button_mod.clicked.connect(lambda state, operation = "%": self.button_operation_clicked(operation))
-        button_reverse.clicked.connect(lambda state, operation = "1/x": self.button_operation_clicked(operation))
-        button_power.clicked.connect(lambda state, operation = "x^2": self.button_operation_clicked(operation))
-        button_root.clicked.connect(lambda state, operation = "x^.5": self.button_operation_clicked(operation))
+        button_reverse.clicked.connect(self.button_reverse_clicked)
+        button_power.clicked.connect(self.button_power_clicked)
+        button_root.clicked.connect(self.button_root_clicked)
 
         ### 추가연산 버튼을 layout_operation 레이아웃에 추가
         layout_number.addWidget(button_mod, 0, 0)
@@ -87,24 +82,23 @@ class Main(QDialog):
             number_button_dict[number].clicked.connect(lambda state, num = number:
                                                        self.number_button_clicked(num))
             if number >0:
-                x,y = divmod(number-1+6, 3)
-                layout_number.addWidget(number_button_dict[number], x, y)
+                x,y = divmod(number - 1, 3)
+                layout_number.addWidget(number_button_dict[number], 4 - x, y)
             elif number==0:
                 layout_number.addWidget(number_button_dict[number], 5, 1)
 
-        ### 소숫점 버튼과 00 버튼을 입력하고 시그널 설정
+        ### 소숫점 버튼과 +/- 버튼을 입력하고 시그널 설정
         button_dot = QPushButton(".")
         button_dot.clicked.connect(lambda state, num = ".": self.number_button_clicked(num))
         layout_number.addWidget(button_dot, 5, 2)
 
-        button_double_zero = QPushButton("00")
-        button_double_zero.clicked.connect(lambda state, num = "00": self.number_button_clicked(num))
-        layout_number.addWidget(button_double_zero, 5, 0)
+        button_plinus = QPushButton("+/-")
+        button_plinus.clicked.connect(self.button_plinus_clicked)
+        layout_number.addWidget(button_plinus, 5, 0)
 
         ### 각 레이아웃을 main_layout 레이아웃에 추가
         main_layout.addLayout(layout_equation_solution)
         main_layout.addLayout(layout_operation)
-        main_layout.addLayout(layout_clear_equal)
         main_layout.addLayout(layout_number)
 
         self.setLayout(main_layout)
